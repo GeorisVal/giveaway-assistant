e1484a51a22d783268fea1972407b6a14eb9405e
l4ur4nn3 <nicolaslauranne99@gmail.com>
2023-01-14 17:26:02 +0100

    temp design files 'donationslist' -- wip

--- resources/js/Pages/Welcomecopy.jsx
+++ resources/js/Pages/Welcomecopy.jsx
@@ -3,7 +3,8 @@ import { Link, Head } from '@inertiajs/inertia-react';
 export default function Welcome(props) {
     return (
         <>
-        
+        <Head title="Donations' List" />
+        <div className='mx-20'>
         <header>
             <nav className="bg-white border-gray-200 py-2.5 rounded dark:bg-black-550">
                 <div className="flex flex-wrap items-center w-full">
@@ -37,82 +38,82 @@ export default function Welcome(props) {
         <main>
             <h2 className="text-2xl">Welcome back, Idris.</h2>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-black-550 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Timestamp
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Status
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Notes
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Scheduled date
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Platform
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Contact
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                NA name
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Discord ID
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Currencies
-                            </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
-                                Items
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-black-550 dark:border-gray-700">
-                            <td className="px-6 py-2">
-                                <a href="#" className="font-medium text-lightgreen-500 dark:text-blue-500 hover:underline"><i className="fa-solid fa-pen-to-square"></i></a>
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                12/01
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                Collected
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                passed to Luna
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                12/02
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                Nookazon
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                Contact
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                Nisune
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                550145862148625
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                2 bells
-                            </td>
-                            <td className="px-6 py-4 truncate ...">
-                                2 Snowflakes Leaves, 3 Maple Leaves, ...
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-black-550 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Timestamp
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Status
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Notes
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Scheduled date
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Platform
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Contact
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    NA name
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Discord ID
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Currencies
+                                </th>
+                                <th scope="col" className="px-6 py-3 truncate ...">
+                                    Items
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-black-550 dark:border-gray-700">
+                                <td className="px-6 py-2">
+                                    <a href="#" className="font-medium text-lightgreen-500 dark:text-blue-500 hover:underline"><i className="fa-solid fa-pen-to-square"></i></a>
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    12/01
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    Collected
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    passed to Luna
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    12/02
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    Nookazon
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    Contact
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    Nisune
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    550145862148625
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    2 bells
+                                </td>
+                                <td className="px-6 py-4 truncate ...">
+                                    2 Snowflakes Leaves, 3 Maple Leaves, ...
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
             </div>
         </main>
         <footer>
@@ -122,6 +123,7 @@ export default function Welcome(props) {
                 <button type="button" className="text-white bg-lightgreen-500 hover:bg-lightgreen-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Scheduled giveaways</button>
             </div>
         </footer>
+        </div>
         </>
     );
 }
