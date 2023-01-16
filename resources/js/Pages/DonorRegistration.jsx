963eb908004918df90e1a63f082dd69521c4a179
l4ur4nn3 <nicolaslauranne99@gmail.com>
2023-01-16 16:54:28 +0100

    wip donor registration

--- resources/js/Pages/DonorRegistration.jsx
+++ resources/js/Pages/DonorRegistration.jsx
@@ -8,7 +8,7 @@ export default function DonorRegister(props) {
         <header>
             <nav className="bg-white border-gray-200 py-2.5 rounded dark:bg-black-550">
                 <div className="flex flex-wrap contactway-center w-full">
-                <div className="flex">
+                    <div className="flex">
                         <Link href="/home" className="flex items-center">
                             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Giftaway</span>
                         </Link>
@@ -38,7 +38,7 @@ export default function DonorRegister(props) {
             </nav>
         </header>
         <main>
-            <form>
+            <form className="mx-96">
                 <div className="mb-6">
                     <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NA Username or Discord ID</label>
                     <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required></input>
@@ -47,13 +47,16 @@ export default function DonorRegister(props) {
                     <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email (optional)</label>
                     <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email"></input>
                 </div>
-                <div className="mb-6">
-                    <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preferred time for the exchange</label>
-                    <input type="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discord ID" required></input>
-                </div>
-                <div className="mb-6">
-                    <label for="time" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                    <input type="time" id="time" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discord or NA" required></input>
+                <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preferred time for the exchange</p>
+                <div className='flex flex-row'>
+                    <div className="mb-6">
+                        <label for="date"  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                        <input type="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discord ID" required></input>
+                    </div>
+                    <div className="mb-6">
+                        <label for="time" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                        <input type="time" id="time" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discord or NA" required></input>
+                    </div>
                 </div>
                 <div className="mb-6">
                     <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preferred way to be contacted</p>
@@ -68,7 +71,7 @@ export default function DonorRegister(props) {
                         </div>
                     </div>
                 </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" className="text-white bg-lightgreen-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
         </main>
         </div>
