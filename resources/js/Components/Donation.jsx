bc4e73378edc0fb11e02c76b330e705ae2dae06c
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2023-01-21 17:46:18 +0100

    Correct

--- resources/js/Components/Donation.jsx
+++ resources/js/Components/Donation.jsx
@@ -1,71 +1,71 @@
-import React from 'react';
-import {useForm, usePage} from '@inertiajs/react'
-
+import React from "react";
+import { useForm, usePage } from "@inertiajs/react";
+import axios from "axios";
 const Donation = (props) => {
-    const {data, setData, put, processing, errors} = useForm({
-        id: props.id,
+    // API call to update the status of a donation
+    const [data, setData] = React.useState({
         status: props.status,
-    })
+    });
 
-    function handleChange(e) {
-        setData('status', e.target.value);
-        console.log("Changed")
-    }
-    function handleSubmit(e) {
-        e.preventDefault()
-        put('/donations/'+props.id, props.status)
-        console.log("Submitted")
-    }
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value });
+        axios
+            .put("/api/donations/" + props.id, {
+                status: e.target.value,
+            })
+            .then((response) => {
+                console.log(response);
+            });
+    };
+
     return (
-    <tr className="bg-white border-b leading-tight">
-        <td className="px-6 py-2">
-            <a href="#" className="font-medium text-lightgreen-500 hover:underline"><i className="fa-solid fa-pen-to-square"></i></a>
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.timestamp}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            <form id={"form"+props.id} action={"/donations"} method={"put"} onChange={handleSubmit}>
-                <select name="status" id={"status" + props.id} value={data.status} defaultValue={props.status} className="leading-3" errors={errors.status} onChange={handleChange}>
-                    <option value="invalid">Invalid</option>
-                    <option value="dnr">DNR</option>
-                    <option value="contacted">Contacted</option>
-                    <option value="pending_pickup">Pending Pickup</option>
-                    <option value="collected">Collected</option>
-                    <option value="scheduled_web">Scheduled Web</option>
-                    <option value="scheduled_discord">Scheduled Discord</option>
-                    <option value="scheduled_programs">Scheduled Programs</option>
-                    <option value="done">Done</option>
-                    <option value="cancelled">Cancelled</option>
-                </select>
-            </form>
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.notes}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.schedule_date}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.platform}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.contact_method}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.discord_username}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.discord_id}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.currencies}
-        </td>
-        <td className="px-6 py-4 truncate ...">
-            {props.items}
-        </td>
-    </tr>
+        <tr className="bg-white border-b leading-tight">
+            <td className="px-6 py-2">
+                <a
+                    href="#"
+                    className="font-medium text-lightgreen-500 hover:underline"
+                >
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </a>
+            </td>
+            <td className="px-6 py-4 truncate ...">{props.timestamp}</td>
+            <td className="px-6 py-4 truncate ...">
+                <form id={"form" + props.id}>
+                    <select
+                        name="status"
+                        id={"status" + props.id}
+                        value={data.status}
+                        defaultValue={props.status}
+                        className="leading-3"
+                        onChange={handleChange}
+                    >
+                        <option value="invalid">Invalid</option>
+                        <option value="dnr">DNR</option>
+                        <option value="contacted">Contacted</option>
+                        <option value="pending_pickup">Pending Pickup</option>
+                        <option value="collected">Collected</option>
+                        <option value="scheduled_web">Scheduled Web</option>
+                        <option value="scheduled_discord">
+                            Scheduled Discord
+                        </option>
+                        <option value="scheduled_programs">
+                            Scheduled Programs
+                        </option>
+                        <option value="done">Done</option>
+                        <option value="cancelled">Cancelled</option>
+                    </select>
+                </form>
+            </td>
+            <td className="px-6 py-4 truncate ...">{props.notes}</td>
+            <td className="px-6 py-4 truncate ...">{props.schedule_date}</td>
+            <td className="px-6 py-4 truncate ...">{props.platform}</td>
+            <td className="px-6 py-4 truncate ...">{props.contact_method}</td>
+            <td className="px-6 py-4 truncate ...">{props.discord_username}</td>
+            <td className="px-6 py-4 truncate ...">{props.discord_id}</td>
+            <td className="px-6 py-4 truncate ...">{props.currencies}</td>
+            <td className="px-6 py-4 truncate ...">{props.items}</td>
+        </tr>
     );
-}
+};
 
 export default Donation;
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DonationController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/donations', [DonationController::class, 'indexAPI']);
+Route::get('/donations/{id}', [DonationController::class, 'showAPI']);
+Route::put('/donations/{id}', [DonationController::class, 'updateAPI']);
