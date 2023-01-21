dfda312abc75c195f23936945c03d37be426497c
Valentin Georis <108730360+GeorisVal@users.noreply.github.com>
2023-01-21 19:29:44 +0100

    Update Donation.jsx
    Fixed Prettier wrongdoing

--- resources/js/Components/Donation.jsx
+++ resources/js/Components/Donation.jsx
@@ -21,49 +21,53 @@ const Donation = (props) => {
     return (
         <tr className="bg-white border-b leading-tight">
             <td className="px-6 py-2">
-                <a
-                    href="#"
-                    className="font-medium text-lightgreen-500 hover:underline"
-                >
+                <a href="#" className="font-medium text-lightgreen-500 hover:underline">
                     <i className="fa-solid fa-pen-to-square"></i>
                 </a>
             </td>
-            <td className="px-6 py-4 truncate ...">{props.timestamp}</td>
+            <td className="px-6 py-4 truncate ...">
+                {props.timestamp}
+            </td>
             <td className="px-6 py-4 truncate ...">
                 <form id={"form" + props.id}>
-                    <select
-                        name="status"
-                        id={"status" + props.id}
-                        value={data.status}
-                        defaultValue={props.status}
-                        className="leading-3"
-                        onChange={handleChange}
-                    >
+                    <select name="status" id={"status" + props.id} value={data.status} defaultValue={props.status} className="leading-3" onChange={handleChange}>
                         <option value="invalid">Invalid</option>
                         <option value="dnr">DNR</option>
                         <option value="contacted">Contacted</option>
                         <option value="pending_pickup">Pending Pickup</option>
                         <option value="collected">Collected</option>
                         <option value="scheduled_web">Scheduled Web</option>
-                        <option value="scheduled_discord">
-                            Scheduled Discord
-                        </option>
-                        <option value="scheduled_programs">
-                            Scheduled Programs
-                        </option>
+                        <option value="scheduled_discord">Scheduled Discord</option>
+                        <option value="scheduled_programs">Scheduled Programs</option>
                         <option value="done">Done</option>
                         <option value="cancelled">Cancelled</option>
                     </select>
                 </form>
             </td>
-            <td className="px-6 py-4 truncate ...">{props.notes}</td>
-            <td className="px-6 py-4 truncate ...">{props.schedule_date}</td>
-            <td className="px-6 py-4 truncate ...">{props.platform}</td>
-            <td className="px-6 py-4 truncate ...">{props.contact_method}</td>
-            <td className="px-6 py-4 truncate ...">{props.discord_username}</td>
-            <td className="px-6 py-4 truncate ...">{props.discord_id}</td>
-            <td className="px-6 py-4 truncate ...">{props.currencies}</td>
-            <td className="px-6 py-4 truncate ...">{props.items}</td>
+            <td className="px-6 py-4 truncate ...">
+                {props.notes}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.schedule_date}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.platform}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.contact_method}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.discord_username}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.discord_id}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.currencies}
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                {props.items}
+            </td>
         </tr>
     );
 };
