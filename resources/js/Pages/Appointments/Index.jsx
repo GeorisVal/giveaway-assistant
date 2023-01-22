18cc79bd45b4e7e0ee16a24426b0d3ba540be710
GeorisVal <georis.val@gmail.com>
2023-01-22 15:43:48 +0100

    Added Appointments infrastructure

---
+++ resources/js/Pages/Appointments/Index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Appointment from '@/Components/Appointment';
+import NavLink from '@/Components/NavLink'
+import Buttons from '@/Components/Buttons';
+import Footer from '@/Components/Footer';
+import { useForm, Head } from '@inertiajs/react';
+
+export default function Index({ appointments }) {
+    return (
+        <>
+            <div className='mx-20'>
+                <NavLink></NavLink>
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left text-gray-500">
+                        <thead className="text-xs text-black-550 uppercase bg-gray-50">
+                        <tr>
+                            <th scope="col" className="px-6 py-3 truncate ...">
+                                Meeting with
+                            </th>
+                            <th scope="col" className="px-6 py-3 truncate ...">
+                                Meeting time
+                            </th>
+                            <th scope="col" className="px-6 py-3 truncate ...">
+                                Contact via
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {appointments.map((appointment) =>
+                            <Appointment key={appointment.id} id={appointment.id} appointment={appointment.appointment} username={appointment.username} contact={appointment.contact_method} />)};
+                        </tbody>
+                    </table>
+                </div>
+                <Buttons></Buttons>
+                <Footer></Footer>
+            </div>
+        </>
+    );
+}
