0799214adc51e856968d3ce9dfa03994c24e8d4f
GeorisVal <georis.val@gmail.com>
2023-01-22 13:59:45 +0100

    Updated Factory to generate better results

--- database/factories/donationFactory.php
+++ database/factories/donationFactory.php
@@ -19,14 +19,15 @@ class donationFactory extends Factory
         return [
             'timestamp' => now(),
             'status' => fake()->randomElement(['invalid', 'dnr', 'contacted', 'pending_pickup', 'collected', 'scheduled_web', 'scheduled_discord', 'scheduled_programs', 'done', 'cancelled']),
+            'schedule_date' => fake()->dateTimeThisMonth(),
             'platform' => fake()->randomElement(['Discord', 'Website']),
             'shoutout' => fake()->randomElement(['1', '0']),
             'contact_method' => fake()->randomElement(['Discord', 'Website']),
-            'discord_username' => fake()->userName(),
+            'discord_username' => fake()->userName().'#'.fake()->numberBetween(0000, 9999),
             'discord_id' => fake()->numberBetween(1000000000000000, 11000000000000000),
             'nookazon_username' => fake()->userName(),
-            'nookazon_link' => 'https://nookazon.com/profile/',
-            'currencies' => 'none',
+            'nookazon_link' => 'https://nookazon.com/profile/'.fake()->numberBetween(0000000000, 9999999999),
+            'currencies' => fake()->numberBetween(100000, 100000000).' Bells',
             'items' => fake()->text(50)
         ];
     }
