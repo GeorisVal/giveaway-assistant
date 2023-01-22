cfb4d8e515152152f7b4ce6604ab06c21931ab36
GeorisVal <georis.val@gmail.com>
2023-01-22 13:44:56 +0100

    Updated table + functionnal notes

--- resources/js/Components/Donation.jsx
+++ resources/js/Components/Donation.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { useForm, usePage } from "@inertiajs/react";
 import axios from "axios";
+import moment from 'moment';
+
 const Donation = (props) => {
     // API call to update the status of a donation
     const [data, setData] = React.useState({
         status: props.status,
     });
+    const [note, setNote] = React.useState({note: props.notes});
+    {/*{moment(props.schedule_date).format('MMMM Do YYYY')}*/}
 
-    const handleChange = (e) => {
+    const handleStatusChange = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
         axios
             .put("/api/donations/" + props.id, {
                 status: e.target.value,
+            })
+            .then((response) => {
+                console.log(response);
+            });
+    };
+    const handleNoteSubmit = (e) => {
+        e.preventDefault()
+        axios
+            .put("/api/donations/" + props.id, {
+                notes: note,
             })
             .then((response) => {
                 console.log(response);
@@ -30,7 +43,7 @@ const Donation = (props) => {
             </td>
             <td className="px-6 py-4 truncate ...">
                 <form id={"form" + props.id}>
-                    <select name="status" id={"status" + props.id} value={data.status} defaultValue={props.status} className="leading-3" onChange={handleChange}>
+                    <select name="status" id={"status" + props.id} value={data.status} defaultValue={props.status} className="leading-3" onChange={handleStatusChange}>
                         <option value="invalid">Invalid</option>
                         <option value="dnr">DNR</option>
                         <option value="contacted">Contacted</option>
@@ -45,13 +58,18 @@ const Donation = (props) => {
                 </form>
             </td>
             <td className="px-6 py-4 truncate ...">
-                {props.notes}
+                <form id={"note" + props.id} onSubmit={handleNoteSubmit}>
+                    <input type="text" defaultValue={props.notes} className="leading-3" onChange={e => setNote(e.target.value)} />
+                </form>
             </td>
             <td className="px-6 py-4 truncate ...">
-                {props.schedule_date}
+                {moment(props.schedule_date).format('Do MMM. YYYY')}
             </td>
             <td className="px-6 py-4 truncate ...">
                 {props.platform}
+            </td>
+            <td className="px-6 text-center py-4 truncate ...">
+                {props.shoutout ? 'Yes' : 'No'}
             </td>
             <td className="px-6 py-4 truncate ...">
                 {props.contact_method}
@@ -60,7 +78,10 @@ const Donation = (props) => {
                 {props.discord_username}
             </td>
             <td className="px-6 py-4 truncate ...">
-                {props.discord_id}
+                <a href={"https://discord.com/channels/@me/339868596589035527"}> {props.discord_id}</a>
+            </td>
+            <td className="px-6 py-4 truncate ...">
+                <a href={props.nookazon_link}>{props.nookazon_username}</a>
             </td>
             <td className="px-6 py-4 truncate ...">
                 {props.currencies}
