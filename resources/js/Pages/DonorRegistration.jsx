a32da7fdd2078a84e7d1bcb30e025a17579df37a
GeorisVal <georis.val@gmail.com>
2023-01-23 10:30:32 +0100

    Split Nookazon & Discord usernames, added bottom margin to form

--- resources/js/Pages/DonorRegistration.jsx
+++ resources/js/Pages/DonorRegistration.jsx
@@ -11,10 +11,16 @@ export default function DonorRegister(props) {
         <NavLink></NavLink>
         </header>
         <main>
-            <form className="mx-52 mt-10 sm:max-xl:mb-10 sm:max-xl:mx-0">
+            <form className="mx-52 mt-10 mb-10 sm:max-xl:mb-10 sm:max-xl:mx-0">
                 <div className="mb-6">
-                    <label for="username" className="block mb-2 text-sm font-medium text-gray-900">NA Username or Discord ID</label>
-                    <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500" placeholder="Username" required></input>
+                    <label for="username" className="block mb-2 text-sm font-medium text-gray-900">Nookazon Username</label>
+                    <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500" placeholder="Username"></input>
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Discord Username</label>
+                    <input type="text" id="username"
+                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500"
+                           placeholder="Username"></input>
                 </div>
                 <div className="mb-6">
                     <label for="email" className="block mb-2 text-sm font-medium text-gray-900">email (optional)</label>
