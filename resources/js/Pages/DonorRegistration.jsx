690a3ff445d14bc5c993760ecb76f559dd3f12a4
GeorisVal <georis.val@gmail.com>
2023-01-24 15:42:42 +0100

    Added date limitation to the date input

--- resources/js/Pages/DonorRegistration.jsx
+++ resources/js/Pages/DonorRegistration.jsx
@@ -1,6 +1,7 @@
 import { Head, useForm } from '@inertiajs/react';
 import NavLink from '@/Components/NavLink';
 import Footer from '@/Components/Footer';
+import moment from "moment/moment";
 
 
 export default function DonorRegister() {
@@ -16,6 +17,8 @@ export default function DonorRegister() {
         e.preventDefault();
         post('/appointments')
     }
+
+    const today = moment(new Date()).format("YYYY-MM-DD");
     return (
         <>
         <Head title="Donor Registration" />
@@ -43,11 +46,11 @@ export default function DonorRegister() {
                 <div className='flex flex-row justify-between'>
                     <div className="mb-6 w-full mr-2">
                         <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900"></label>
-                        <input type="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500" placeholder="" required value={data.appointment_date} onChange={e => setData('appointment_date', e.target.value)} />
+                        <input type="date" id="date" min={today} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500" placeholder="" required value={data.appointment_date} onChange={e => setData('appointment_date', e.target.value)} />
                     </div>
                     <div className="mb-6 w-full mr-2">
                         <label htmlFor="time" className="block mb-2 text-sm font-medium text-gray-900"></label>
-                        <input type="time" id="time" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500" placeholder="" required value={data.appointment_time} onChange={e => setData('appointment_time', e.target.value)} />
+                        <input type="time" id="time" min="13:00" max="21:30" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.50y-600r-gray-400-blue-500er-blue-500" placeholder="" required value={data.appointment_time} onChange={e => setData('appointment_time', e.target.value)} />
                     </div>
                 </div>
                 <div className="mb-6">
