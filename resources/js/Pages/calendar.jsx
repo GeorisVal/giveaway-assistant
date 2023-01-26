53f3ef498e0af5955d04e9cba4fd0554347d7143
Vittoria-P <vittoriapellizzari.pro@gmail.com>
2023-01-26 10:54:05 +0100

    Add visuals of the calendar need to check

--- resources/js/Pages/calendar.jsx
+++ resources/js/Pages/calendar.jsx
@@ -4,15 +4,9 @@ import { Link, Head } from "@inertiajs/react";
 import NavLink from "@/Components/NavLink";
 import Footer from "@/Components/Footer";
 
-<<<<<<< HEAD
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-=======
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'resources/css/Calendar.css';
->>>>>>> 61edae414278b829581a479a81c7160d33645ed2
 
 export default function MyApp() {
     const [value, onChange] = useState(new Date());
@@ -20,7 +14,7 @@ export default function MyApp() {
     return (
         <>
             <NavLink></NavLink>
-            <div>
+            <div class="calendar">
                 <Calendar onChange={onChange} value={value} />
             </div>
         </>
