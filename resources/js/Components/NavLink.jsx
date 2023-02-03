a407593497e2485576473cb8090b1546edf195e6
l4ur4nn3 <nicolaslauranne99@gmail.com>
2023-02-03 12:42:07 +0100

    temp

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -5,7 +5,8 @@ import SecondaryButton from '@/Components/SecondaryButton';
 
 export default function NavLink(props) {
     if(props.auth.user != null) {
-        console.log(props);
+        // console.log(props);
+        // console.log(props.auth.user.name);
         return (
             <>
                 <nav className="bg-white border-gray-200 py-2.5 rounded">
@@ -48,6 +49,7 @@ export default function NavLink(props) {
                         </div>
                     </div>
                 </nav>
+                <p className='my-5 text-xl ml-5'>Hello, <span className='capitalize text-black-550'>{props.auth.user.name}</span></p>
             </>
         )
     }
@@ -67,6 +69,13 @@ export default function NavLink(props) {
                         </div>
                         <div className="w-full md:block md:w-auto" id="navbar-default">
                             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+                                <li>
+                                    <Dropdown.Link href="/" aria-current="page">
+                                        <PrimaryButton>
+                                            Home
+                                        </PrimaryButton>
+                                    </Dropdown.Link>
+                                </li>
                                 <li>
                                     <Dropdown.Link href="/login" as="button">
                                         <SecondaryButton>
--- resources/js/Pages/Donations/Index.jsx
+++ resources/js/Pages/Donations/Index.jsx
@@ -7,12 +7,15 @@ import Buttons from '@/Components/Buttons';
 import { Head, useForm } from '@inertiajs/react';
 
 export default function Index( {donations, status} ) {
+    console.log(donations);
     return (
         <>
             <Head title="Items List" />
         <div className='mx-20'>
             <header>
-                <NavLink auth={{user: "logged"}}></NavLink>
+                <NavLink 
+                auth={{user: "logged"}}
+                ></NavLink>
             </header>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left text-gray-500">
@@ -69,4 +72,5 @@ export default function Index( {donations, status} ) {
         </div>
         </>
     );
+    // console.log(props);
 }
