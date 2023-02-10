2a739549a511b4abb31567c47c26ee2b3e730e73
GeorisVal <georis.val@gmail.com>
2023-02-10 15:22:58 +0100

    Removed bottom buttons, added auth wall & no-date button

--- resources/js/Pages/Calendar/Index.jsx
+++ resources/js/Pages/Calendar/Index.jsx
@@ -1,11 +1,21 @@
 import Calendar from "@/Components/Calendar";
 import NavLink from '@/Components/NavLink';
-import { Head } from '@inertiajs/react';
-import Buttons from "@/Components/Buttons";
+import {Head, Link} from '@inertiajs/react';
 import Responsive from "@/Components/Responsive";
+import SecondaryButton from "@/Components/SecondaryButton";
 
 export default function CalendarPage(props) {
-    console.log(props);
+    function checkauth() {
+        if (props.auth.user != null) {
+            return (
+                <Link href={'donations-nodate'}>
+                    <SecondaryButton>No Date!</SecondaryButton>
+                </Link>)
+        }
+        else {
+            return "";
+        }
+    }
     return (
         <>
             <Head title="Calendar" />
@@ -15,10 +25,11 @@ export default function CalendarPage(props) {
                     <Calendar auth={props.auth} appointments={props.appointments} donations={props.donations}/>
                     <hr />
                     <Calendar auth={props.auth} appointments={props.appointments} donations={props.donations} isNext={1}/>
-                    <div className="my-4 mb-10"></div>
-                    <Buttons></Buttons>
                 </div>
+                <div className="mb-6">
+                    {checkauth()}
                 <Responsive />
+                </div>
             </div>
         </>
     )
