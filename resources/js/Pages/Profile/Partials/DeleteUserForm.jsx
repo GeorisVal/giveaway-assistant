4661cf9edd419895a7001928a3443b3b6424e8ce
GeorisVal <georis.val@gmail.com>
2023-02-10 12:26:33 +0100

    Working Build

--- resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from 'react';
-import DangerButton from '@/Components/DangerButton';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import Modal from '@/Components/Modal';
