32536cfb361793285bf69ef7d7f884ecbd8bf837
GeorisVal <georis.val@gmail.com>
2023-03-12 14:31:33 +0100

    Sticky bottom buttons

--- resources/js/Components/Buttons.jsx
+++ resources/js/Components/Buttons.jsx
@@ -5,10 +5,10 @@ import SecondaryButton from '@/Components/SecondaryButton';
 export default function Buttons() {
     return (
         <>
-            <div className="my-4 mb-10">
+            <div className={"my-4 mb-10 sticky bottom-0 bg-white"}>
                 <Link href={'donations'}><PrimaryButton>Donations</PrimaryButton></Link>
                 <Link href={'donations-nodate'}><SecondaryButton>No date!</SecondaryButton></Link>
-                <Link href={'appointments'}><PrimaryButton>Scheduled giveaways</PrimaryButton></Link>
+                <Link href={'donations-scheduled'}><PrimaryButton>Scheduled giveaways</PrimaryButton></Link>
             </div>
         </>
     );
