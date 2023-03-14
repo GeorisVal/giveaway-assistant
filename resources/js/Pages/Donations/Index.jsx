ca90ef504d86c60e595855d3600ace34245b8d32
GeorisVal <georis.val@gmail.com>
2023-03-14 09:12:52 +0100

    Fixed table code formatting to match rest of headers

--- resources/js/Pages/Donations/Index.jsx
+++ resources/js/Pages/Donations/Index.jsx
@@ -21,7 +21,8 @@ export default function Index( {donations, status, auth, link, platform} ) {
                 <table className="w-full text-sm text-left text-gray-500">
                     <thead className="text-xs text-black-550 uppercase bg-gray-50">
                     <tr>
-                        <th scope="col" className="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
+
                     </th>
                     <th scope="col" className="px-6 py-3 truncate cursor-default ...">
                         Date
