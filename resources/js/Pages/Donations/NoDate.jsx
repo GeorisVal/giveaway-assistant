ec956c7d411802d9dd5620f798dd1f918af548d4
GeorisVal <georis.val@gmail.com>
2023-03-16 21:29:54 +0100

    Stickied top row & limited table view height

--- resources/js/Pages/Donations/NoDate.jsx
+++ resources/js/Pages/Donations/NoDate.jsx
@@ -7,6 +7,7 @@ import Responsive from '@/Components/Responsive';
 import FilterPlatform from "@/Components/PlatformList";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Filters from "@/Components/StatusList";
 
 export default function Index({ donations, platform }) {
     return (
@@ -17,17 +18,18 @@ export default function Index({ donations, platform }) {
                     <NavLink auth={{user: "logged"}}></NavLink>
                 </header>
                 <ToastContainer autoClose={1000} position={"top-center"}/>
-                <div className="max-md:hidden">
+                <div className="relative overflow-x-scroll overflow-y-scroll max-h-[68vh] shadow-md sm:rounded-lg">
                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table className="w-full text-sm text-left text-gray-500">
-                        <thead className="text-xs text-black-550 uppercase bg-gray-50">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
+                        <thead className="text-xs text-black-550 uppercase bg-gray-50 z-20 sticky top-0">
+                            <tr>
+                            <th scope="col" className="px-6 py-3 bg-gray-50 sticky left-0">
+
                             </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
+                            <th scope="col" className="px-6 py-3 truncate bg-gray-50 sticky left-[3rem] cursor-default ...">
                                 Date
                             </th>
-                            <th scope="col" className="px-6 py-3 truncate ...">
+                            <th scope="col" className="px-6 py-3 truncate bg-gray-50 cursor-default sticky left-[9.5rem] ...">
                                 Status
                             </th>
                             <th scope="col" className="px-6 py-3 truncate ...">
