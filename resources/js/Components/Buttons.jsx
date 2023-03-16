9883c32ea99c0b48ffbf3efb5d1171e1adb772c2
GeorisVal <georis.val@gmail.com>
2023-03-16 21:28:58 +0100

    Removed white BG

--- resources/js/Components/Buttons.jsx
+++ resources/js/Components/Buttons.jsx
@@ -5,7 +5,7 @@ import SecondaryButton from '@/Components/SecondaryButton';
 export default function Buttons() {
     return (
         <>
-            <div className={"my-4 mb-10 sticky bottom-0 bg-white"}>
+            <div className={"my-4 mb-10 sticky bottom-0 z-30"}>
                 <Link href={'donations'}><PrimaryButton>Donations</PrimaryButton></Link>
                 <Link href={'donations-nodate'}><SecondaryButton>No date!</SecondaryButton></Link>
                 <Link href={'donations-scheduled'}><PrimaryButton>Scheduled giveaways</PrimaryButton></Link>
