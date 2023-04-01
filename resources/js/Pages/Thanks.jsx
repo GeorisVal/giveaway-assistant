88fe44b7affb47458fdc47c4a2d1f808bfda8188
Valentin Georis <georis.val@gmail.com>
2023-04-01 17:54:22 +0200

    Removed responsive limitation

--- resources/js/Pages/Thanks.jsx
+++ resources/js/Pages/Thanks.jsx
@@ -11,14 +11,13 @@ export default function Thanks(props) {
     return (
         <>
             <Head title="Thanks" />
-            <div className='mx-20'>
+            <div className='mx-15'>
                 <NavLink auth={props.auth}></NavLink>
-                <div className="max-md:hidden mt-20 flex flex-col w-full mx-auto items-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+                <div className="mt-20 flex flex-col w-full mx-auto items-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-black-550">Request sent!</h5>
                     <p className="font-normal text-black-550">Thank you for taking the time to schedule a time with our team ! We will contact you in due time with the informations you have provided.</p>
                 </div>
             </div>
-            <Responsive />
         </>
     );
 }
