330c087b7b59ae5693b8be28a98dac033c72a4f8
Valentin Georis <georis.val@gmail.com>
2023-04-01 17:55:44 +0200

    Fixed shoutout to match actual DB

--- database/factories/donationFactory.php
+++ database/factories/donationFactory.php
@@ -20,7 +20,7 @@ class donationFactory extends Factory
             'status' => fake()->randomElement(['Invalid', 'Did Not Respond', 'Donator Contacted', 'Pending pick up', 'Items Collected', 'Queued for Website', 'Queued for Discord', 'Queued for Programs', 'Completed', 'Cancelled', '']),
             'schedule_date' => fake()->randomElement([null, fake()->dateTimeThisMonth()]),
             'platform' => fake()->randomElement(['Nookazon Discord', 'Nookazon Website', 'Nookazon Programs (i.e. Streams, Contests, etc.)']),
-            'shoutout' => fake()->randomElement(['Yes - shout out my Nookazon account', 'Yes - shout out my Discord account', 'No - I would like to remain anonymous']),
+            'shoutout' => fake()->randomElement(['Yes - Shout out my Nookazon account', 'Yes - Shout out my Discord account', 'No - I would like to remain anonymous']),
             'contact_method' => fake()->randomElement(['Discord', 'Website']),
             'discord_username' => fake()->userName().'#'.fake()->numberBetween(1000, 9999),
             'discord_id' => fake()->numberBetween(1000000000000000, 11000000000000000),
