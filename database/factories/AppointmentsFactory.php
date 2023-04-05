2b6fc5bd950809c32e42829c42aa2b84c08f2dec
Valentin Georis <georis.val@gmail.com>
2023-04-05 23:11:51 +0200

    Split time to time start/end

--- database/factories/AppointmentsFactory.php
+++ database/factories/AppointmentsFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Appointments>
@@ -16,11 +17,14 @@ class AppointmentsFactory extends Factory
      */
     public function definition()
     {
+        $time = fake()->time();
+        $timeTwo = Carbon::createFromFormat('H:i:s', $time)->addMinutes(30);
         return [
         'nookazon_username' => fake()->userName(),
         'discord_username' => fake()->userName().'#'.fake()->numberBetween(1000, 9999),
         'appointment_date' => fake()->dateTimeThisMonth(),
-        'appointment_time' => fake()->time('H:i:s'),
+        'appointment_time_start' => $time,
+        'appointment_time_end' => $timeTwo,
         'contact_method' => fake()->randomElement(['Discord', 'Website']),
         'appointment_type' => fake()->randomElement(['donor', 'winner'])
     ];
