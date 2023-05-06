1262cf519a17719066b69169c0594e08f228d96d
Valentin Georis <georis.val@gmail.com>
2023-05-06 19:32:31 +0200

    Added ability to edit status on giveaway details (calendar)

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -98,7 +98,7 @@ class DonationController extends Controller
 
     public function updateGiveawayDetailsAPI(Request $request, $date)
     {
-        $data = $request->validate(['title' => 'nullable', 'img_link' => 'nullable', 'description' => 'nullable', 'schedule_date' => 'required']);
+        $data = $request->validate(['title' => 'nullable', 'img_link' => 'nullable', 'description' => 'nullable', 'schedule_date' => 'required', 'status' => 'nullable']);
         DB::table('donations')
             ->where('schedule_date', $date)
             ->update($data);
