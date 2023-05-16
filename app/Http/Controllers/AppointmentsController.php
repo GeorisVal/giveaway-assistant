4743cf9bbde07cbcbcc50f267c0a4c155c12e3c4
Valentin Georis <georis.val@gmail.com>
2023-05-16 17:46:01 +0200

    Shows Giveaway Live, Winner Contacted & Completed on calendar

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -144,12 +144,18 @@ class AppointmentsController extends Controller
         return Inertia::render('Calendar/Index', [
             'appointments' => DB::table('appointments')->get()
         ] + ['donations' => DB::table('donations')
-                ->select('status', 'schedule_date', 'description', 'title', 'img_link', 'currencies', 'items', 'shoutout', 'nookazon_username', 'nookazon_link', 'discord_username', 'discord_id')
+                ->select('status', 'schedule_date', 'description', 'title', 'img_link', 'platform', 'currencies', 'items', 'shoutout', 'nookazon_username', 'nookazon_link', 'discord_username', 'discord_id')
                 ->where('status', '=', 'Queued for Website')
                 ->whereNotNull('schedule_date')
                 ->orWhere('status', '=',  'Queued for Discord')
                 ->whereNotNull('schedule_date')
                 ->orWhere('status', '=',  'Queued for Programs')
+                ->whereNotNull('schedule_date')
+                ->orwhere('status', '=', 'Giveaway Live')
+                ->whereNotNull('schedule_date')
+                ->orwhere('status', '=', 'Winner Contacted')
+                ->whereNotNull('schedule_date')
+                ->orwhere('status', '=', 'Completed')
                 ->whereNotNull('schedule_date')
                 ->get()]);
     }
